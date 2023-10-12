347d8856be736afb11c32c3a55697b13e8413e30
Ashok <bayes.ashok@gmail.com>
2023-10-12 21:09:47 +0545

    added all post and get

--- src/main/java/com/example/demo/Repository/ItemRepository.java
+++ src/main/java/com/example/demo/Repository/ItemRepository.java
@@ -1,2 +1,10 @@
-package com.example.demo.Repository;public interface ItemRepository {
+package com.example.demo.Repository;
+
+import com.example.demo.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
--- src/main/java/com/example/demo/service/impl/ItemServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ItemServiceImpl.java
@@ -1,14 +1,22 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.Pojo.ItemPojo;
+import com.example.demo.Repository.ItemRepository;
 import com.example.demo.entity.Item;
 import com.example.demo.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-public class ItemServiceimpl implements ItemService {
+@Service
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+    private final ItemRepository itemRepository;
+
+    @Override
     public void saveItem(ItemPojo itemPojo) {
         Item item = new Item();
 
@@ -25,13 +33,18 @@ public class ItemServiceimpl implements ItemService {
         itemRepository.save(item);
     }
 
-    List<Item> getAllItems(){
-        return userRepository.findAll();
+    @Override
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
     }
-    Optional<Item> getItemById(Integer id){
 
+    @Override
+    public Optional<Item> getItemById(Integer id) {
+        return itemRepository.findById(id);
     }
-    void deleteItemById(Integer id){
 
+    @Override
+    public void deleteItemById(Integer id) {
+        itemRepository.deleteById(id);
     }
 }
