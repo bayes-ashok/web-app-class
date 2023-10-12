5ea0867fd55bf122fe28bba21008bec948afbe62
Ashok <bayes.ashok@gmail.com>
2023-10-12 20:50:17 +0545

    pojo for item completed

--- src/main/java/com/example/demo/Pojo/ItemPojo.java
+++ src/main/java/com/example/demo/Pojo/ItemPojo.java
@@ -1,2 +1,22 @@
-package com.example.demo.Pojo;public class ItemPojo {
+package com.example.demo.Pojo;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemPojo {
+    private Integer id;
+
+    @NotEmpty(message = "Item Name required")
+    @NotNull(message = "Item Name required")
+    private String itemName;
+
+    private String description;
+
+    @NotNull(message = "Price required")
+    private Double price;
 }
